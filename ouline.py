2edc8847de6e8011c9f2be2b4ce832a7d3b01616
dmcnei19 <dmcnei19@uncc.edu>
2022-04-04 19:49:00 -0400

    added beautiful soup request

--- ouline.py
+++ ouline.py
@@ -1,5 +1,16 @@
 #import beautifulsoup and request here
+import requests
 
+url = "https://www.indeed.com/jobs?q=Software Developer&l=Charlotte"
+
+payload={}
+headers = {
+  'Cookie': 'CTK=1fvrdvh93ptuu800; INDEED_CSRF_TOKEN=T2ABBZ0uBzuSmcrgE1JgfpDyxQUzUoyJ; JSESSIONID=CE9A0B60F4BFD0F9EB880E945E6B7977; PREF="TM=1649114334507:L=Charlotte"; RQ="q=Software+Developer&l=Charlotte&ts=1649114334532"; UD="LA=1649114334:CV=1649114334:TS=1649114334:SG=af77ddcf45faa77487636e0b17cca30c"; ctkgen=1; indeed_rcc=""; jaSerpCount=1'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
 
 #function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
 def getJobList():
